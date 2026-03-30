89fc1a8127bde1cffb8a7489f7f803213ac7692b
Jamal <jamaly381@gmail.com>
2026-03-30 22:13:58 +0100

    Using js to render projects to the page

--- main.js
+++ main.js
@@ -15,3 +15,65 @@ window.onscroll = ()=>{
     nav.classList.remove('active-nav');
     icon.src = "images/icons8-hamburger-button-50.png";
 }
+
+
+// RENDERING PROJECTS TO THE PAGE FROM JS
+
+const projectsArray = [
+    {
+        name: "Consultant Machine",
+        title: "Machine Helping Company",
+        description: "A Professional Commercial and Residiential Cleaning Service Webisite, built to generate more customers. The website is responsive and mobile friendly.",
+        links: {
+            view: "https//www.machine.com.ng",
+            code: "https//github/mahine.com.ng.git"
+        },
+        image: "images/work-one.png"
+    },
+    {
+        name: "Consultant Machine",
+        title: "Machine Helping Company",
+        description: "A Professional Commercial and Residiential Cleaning Service Webisite, built to generate more customers. The website is responsive and mobile friendly.",
+        links: {
+            view: "https//www.machine.com.ng",
+            code: "https//github/mahine.com.ng.git"
+        },
+        image: "images/work-one.png"
+    }
+];
+
+const projectContainer = document.querySelector(".project-section");
+
+projectContainer.innerHTML = "";
+let className;
+projectsArray.forEach((project, index) => {
+    if(index % 2 === 0){
+        className = "container";
+    }else {
+        className = "container container-two";
+    }
+    const projectHtml = `
+
+        <div class="${className}">
+                <div class="content">
+                    <p class="title">
+                        ${project.name}
+                    </p>
+                    <h2>${project.title}</h2>
+                    <div class="main-content">
+                        <p>${project.description}</p>
+                    </div>
+                    <ul>
+                        <a href="${project.links.view}"><button class="visit-btn">Visit</button></a>
+
+                        <a href="${project.links.code}"><button>Code</button></a>
+                    </ul>
+                </div>
+                <div class="image">
+                    <img src="${project.image}" alt="" class="Portfolio">
+                </div>
+        </div>
+    `;
+    projectContainer.innerHTML += projectHtml;
+    console.log(projectContainer);
+})
